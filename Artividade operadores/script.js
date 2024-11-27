58ae5812dba334cdb8921f50613d791474aaf5f0
Daniel Sena Pessoa <danielsenapessoa12@gmail.com>
2024-11-27 06:04:23 -0300

    Finalizando

---
+++ Artividade operadores/script.js
@@ -0,0 +1,11 @@
+let idade = 30;
+
+console.log("Valor padrão: " + idade);
+
+idade = 30 + 6;
+
+console.log("Valor incrementado: " + idade);
+
+idade = 30 - 6;
+
+console.log("Valor decrementado: " + idade);
