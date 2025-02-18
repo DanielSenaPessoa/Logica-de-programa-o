49cedc07faf7590f33a6db90354a16322c05f32c
DanielSenaPessoa <163361298+DanielSenaPessoa@users.noreply.github.com>
2025-02-18 12:06:14 -0300

    a

--- Artividade operadores/function.js
+++ Artividade operadores/function.js
@@ -1,7 +1,10 @@
-torrar();
+torrar("pão de forma","joão", 5.00);
+torrar("pão integral","maria", 6.00);
 
-function torrar(){
-    console.log("torrando pão");
+function torrar(pao, nome = "cliente", valor ){
+    console.log("torrando feita com " + pao);
+    console.log("ela é um pedido de " + nome);
+    console.log("o valor é de " + valor);
     injetarPao();
 }
 
@@ -9,3 +12,24 @@ function injetarPao(){
     console.log("preparando para injetar o pão");
     console.log("Finalizado");
 }
+console.log("====================================");
+
+let resultado = soma(10, 20);
+
+function soma(numA, numB){
+    let somatorio = numA + numB;
+    return somatorio;
+}
+
+console.log(resultado);
+
+console.log("====================================");
+
+let userName = getFirstName("João da Silva", " ");
+
+function getFirstName(name, splitchar){
+    let firstName = name.split(splitchar)[0];
+    return firstName;
+}
+
+console.log("Seja bem vindo " + userName);
